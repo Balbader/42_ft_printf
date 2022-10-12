f3ff0aeb7c1923ce4df4e7f501558139b115a720
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-12 16:36:23 +0200

    ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 15:05:46 by baalbade          #+#    #+#             */
+/*   Updated: 2022/10/12 15:05:48 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <unistd.h>
+
+#endif 
