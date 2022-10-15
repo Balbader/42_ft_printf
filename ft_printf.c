be5b6afe0b168fd74bde4d29a6885af617b3605a
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-15 13:40:55 +0200

    ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -12,31 +12,37 @@
 
 #include "ft_printf.h"
 
-void	ft_check_sign(char sign, va_list *arg)
+static void	ft_check_sign(char sign, va_list *arg, int *tot_len, int *i)
 {
 	if (sign == 's')
-		ft_print_str(va_arg(*arg, char *));
+		ft_print_str(va_arg(*arg, char *), tot_len);
 	else if (sign == 'd' || sign == 'i')
-		ft_print_nb(va_arg(*arg, int));
+		ft_print_nb(va_arg(*arg, int), tot_len);
 	else if (sign == 'u')
-		ft_print_unsigned(va_arg(*arg, unsigned int));
+		ft_print_unsigned(va_arg(*arg, unsigned int), tot_len);
 	else if (sign == 'x')
-		ft_print_hexa(va_arg(*arg, unsigned int), 'x');
+		ft_print_hexa(va_arg(*arg, unsigned int), 'x', tot_len);
 	else if (sign == 'X')
-		ft_print_hexa(va_arg(*arg, unsigned int), 'X');
+		ft_print_hexa(va_arg(*arg, unsigned int), 'X', tot_len);
 	else if (sign == 'p')
-		ft_print_ptr(va_arg(*arg, size_t));
+		ft_print_ptr(va_arg(*arg, size_t), tot_len);
 	else if (sign == 'c')
-		ft_print_char(va_arg(*arg, int));
+		ft_print_char(va_arg(*arg, int), tot_len);
 	else if (sign == '%')
-		ft_print_char('%');
+		ft_print_char('%', tot_len);
+	else
+		(*i)--;
 }
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	arg;
+	int		tot_len;
 	int		i;
 
+	if (!str)
+		return (0);
+	tot_len = 0;
 	i = 0;
 	va_start(arg, str);
 	while (str[i] != '\0')
@@ -44,15 +50,15 @@ int	ft_printf(const char *str, ...)
 		if (str[i] == '%')
 		{
 			i++;
-			ft_check_sign(str[i], &arg);
+			ft_check_sign(str[i], &arg, &tot_len, &i);
 			// i++;
 		}
 		else
 		{
-			ft_print_char((char)str[i]);
+			ft_print_char((char)str[i], &tot_len);
 			i++;
 		}
 	}
 	va_end(arg);
-	return (0);
+	return (tot_len);
 }
