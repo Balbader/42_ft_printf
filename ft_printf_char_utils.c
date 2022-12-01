cffdb83443f8888db49a8919a2ab5576a7be1e74
Basil ALBADER <baalbade@student.42.fr>
2022-12-01 07:11:53 +0100

    final version - no bonus

---
+++ ft_printf_char_utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_char_utils.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 07:48:15 by baalbade          #+#    #+#             */
+/*   Updated: 2022/11/30 11:16:03 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_print_char(char c, int *tot_len)
+{
+	write(1, &c, 1);
+	(*tot_len)++;
+}
+
+void	ft_print_str(char *str, int *tot_len)
+{
+	size_t	i;
+
+	if (!str)
+	{
+		(*tot_len) = 0;
+		return ;
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_print_char(str[i], tot_len);
+		i++;
+	}
+}
